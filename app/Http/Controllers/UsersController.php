75c96e38b93247367a9b9306134b581342f1c372
LEESHUNHAO <415827122@qq.com>
2018-06-06 15:39:48 +0000

    添加编辑页面身份验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 
 class UsersController extends Controller
@@ -16,6 +17,10 @@ class UsersController extends Controller
 
     public function edit (User $user)
     {
+        if (Auth::id() != $user->id) {
+            $user->id = Auth::id();
+            return  redirect()->route('users.show', $user->id)->with('danger', '非法操作！');
+        }
         return view('users.edit', compact('user'));
     }
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - 茵为有你</title>
+    <title>@yield('title', 'LaraBBS') - 还好有你在</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
