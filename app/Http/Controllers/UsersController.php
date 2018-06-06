f7b14d99e3473f2eec24e9a763be4df9c737fe4d
LEESHUNHAO <415827122@qq.com>
2018-06-06 15:48:27 +0000

    修复未登陆用户试图通过路由进入用户编辑页面时出现报错页面的问题

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,21 +10,26 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
-    public function show (User $user)
+    public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
-    public function edit (User $user)
+    public function edit(User $user)
     {
+        if (!Auth::id()) {
+            return redirect()->route('login');
+        }
+
         if (Auth::id() != $user->id) {
             $user->id = Auth::id();
-            return  redirect()->route('users.show', $user->id)->with('danger', '非法操作！');
+            return redirect()->route('users.show', $user->id)->with('danger', '非法操作！');
         }
+        
         return view('users.edit', compact('user'));
     }
 
-    public function update (UserRequest $request, User $user)
+    public function update(UserRequest $request, User $user)
     {
         $user->update($request->all());
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
