70ee66015e49a1d68a0a0c5167cbe9346eaab1f2
LEESHUNHAO <415827122@qq.com>
2018-07-16 15:09:57 +0000

    小修改

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -19,7 +19,7 @@ class TopicsController extends Controller
 
     public function index(Request $request, Topic $topic)
     {
-        $topics = $topic->withOrder($request->order)->paginate(20);
+        $topics = $topic->where('category_id','!=',5)->withOrder($request->order)->paginate(20);
         return view('topics.index', compact('topics'));
     }
 
