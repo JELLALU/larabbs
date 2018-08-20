466721305da04a947236e12f098de6128652bd70
LEESHUNHAO <415827122@qq.com>
2018-08-20 08:48:39 +0000

    Add manage contents permission and visit horizon dashboard permission

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -44,9 +44,18 @@ class UsersTableSeeder extends Seeder
 
         // 单独处理第一个用户的数据
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@yousails.com';
-        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->name = 'LEESHUNHAO';
+        $user->email = '415827122@qq.com';
+        $user->avatar = 'http://larabbs.test/uploads/images/avatars/201808/09/11_1533800346_N02Xdu7F5M.jpg';
+        $user->password = '$2y$10$Z0iYyOAGIMtYx.9baoWnce1mkz5kqAp7G09UvLBo8nB37kzcvqy7q';
+        $user->introduction = '嗯？';
         $user->save();
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
